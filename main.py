8d49b222b89c2591fd6273527a3301a360a4df73
shaizae <61865612+shaizae@users.noreply.github.com>
2021-12-02 13:46:12 +0200

    final white report and comments

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import cv2
-
+import time
 from utils import *
 
 feature_params = dict(maxCorners=100,
@@ -11,7 +10,6 @@ lk_params = dict(winSize=(15, 15),
                  criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 start_flag = True
 cap = cv2.VideoCapture('test_video.mp4')  # reading the video
-changing_between_frames = []
 while cap.isOpened():  # run on the video
     ret, frame = cap.read()
     if frame is None:
@@ -25,17 +23,15 @@ while cap.isOpened():  # run on the video
     p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, gray, p0, None,
                                            **lk_params)  # calculate the optical flow new location
     changing_inside_frames = mean_frame_changing(p0, p1, st)  # find the mean change between frames
-    old_gray = gray.copy()
-    if cv2.waitKey(1) & 0xFF == ord('w'):
+    old_gray = gray.copy()  # save old frame to comparison
+    if cv2.waitKey(1) & 0xFF == ord('w'):  # stop for deeper look fo frame difference
         show_frame_diff(old_gray, gray, p0, p1, st)
     text = "the frame diff is: " + str(changing_inside_frames)
-    cv2.putText(gray, text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 255, 255))
-    changing_between_frames.append(changing_inside_frames)
+    cv2.putText(gray, text, (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.3,
+                (0, 255, 255))  # print the average distance between etch frame
     cv2.imshow('frame', gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):  # emergency stop of playing
         break
 
-
-mean_chang_between_frames = np.array(changing_between_frames).mean()
 cap.release()
 cv2.destroyAllWindows()
--- utils.py
+++ utils.py
@@ -4,6 +4,13 @@ import cv2 as cv2
 
 
 def mean_frame_changing(p0, p1, st):
+    """
+    finding the mean do all feathers differences
+    :param p0: the best feathers in the old frame
+    :param p1: the best feathers in the new frame
+    :param st: check if the feather exist in p0 and p1
+    :return: the mean different between frames
+    """
     if p1 is not None:
         good_new = p1[st == 1]
         good_old = p0[st == 1]
@@ -18,21 +25,31 @@ def mean_frame_changing(p0, p1, st):
 
 
 def show_frame_diff(old_gray, gray, p0, p1, st):
+    """
+    print two frames and show the differences between them
+    :param old_gray: the older frame
+    :param gray: the current frame
+    :param p0: the best feathers in the old frame
+    :param p1: the best feathers in the new frame
+    :param st: check if the feather exist in p0 and p1
+    :return: None
+    """
     if p1 is not None:
-        good_new = p1[st == 1]
+        good_new = p1[st == 1]  # find only existing points
         good_old = p0[st == 1]
         X_shift = np.squeeze(good_old[:, 0] - good_new[:, 0]).mean()
         Y_shift = np.squeeze(good_old[:, 1] - good_new[:, 1]).mean()
-        X_shift = int(X_shift)
-        Y_shift = int(Y_shift)
+        X_shift = int(X_shift)  # find the mean different in the X label
+        Y_shift = int(Y_shift)  # find the mean different in the Y label
         num_rows, num_cols = old_gray.shape[:2]
         translation_matrix = np.float32([[1, 0, X_shift], [0, 1, Y_shift]])
-        new_pic = cv2.warpAffine(old_gray, translation_matrix, (num_cols, num_rows))
+        new_pic = cv2.warpAffine(old_gray, translation_matrix,
+                                 (num_cols, num_rows))  # moving the old pic to look like the new
         print(f"X: {X_shift}")
         print(f"Y:{Y_shift}")
-
-
-    plt.subplot(1, 3, 1), plt.imshow(old_gray, 'gray')
-    plt.subplot(1, 3, 2), plt.imshow(gray, 'gray')
-    plt.subplot(1, 3, 3), plt.imshow(new_pic, 'gray')
-    plt.show()
+        ## plot the required picther
+        plt.subplot(1, 3, 1), plt.imshow(old_gray, 'gray')
+        plt.subplot(1, 3, 2), plt.imshow(gray, 'gray')
+        plt.subplot(1, 3, 3), plt.imshow(new_pic, 'gray')
+        plt.show()
+        ##
